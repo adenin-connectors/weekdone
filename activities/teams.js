f49c65ef3259d11d254f314b8f64bc5d2c928361
snt_danilo_markovic <dmarkovic@smartnet.rs>
2019-03-27 15:42:57 +0100

    code refactoring. trial expired.

--- activities/teams.js
+++ activities/teams.js
@@ -1,21 +1,16 @@
 'use strict';
-
-const cfActivity = require('@adenin/cf-activity');
 const api = require('./common/api');
 
 module.exports = async function (activity) {
 
   try {
-    api.initialize(activity);
     const response = await api('/teams');
 
-    if (!cfActivity.isResponseOk(activity, response)) {
-      return;
-    }
+    if (Activity.isErrorResponse(response)) return;
 
     activity.Response.Data = convertResponse(response);
   } catch (error) {
-    cfActivity.handleError(activity, error);
+    Activity.handleError(error);
   }
 };
 //**maps response data to items */
