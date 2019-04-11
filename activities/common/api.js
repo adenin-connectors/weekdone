83c664b62195d1afe622110a18c2ffd7163dae35
olliejm <olliejm@protonmail.com>
2019-04-11 16:23:51 +0100

    prepare for azure pipeline

--- activities/common/api.js
+++ activities/common/api.js
@@ -1,4 +1,5 @@
 'use strict';
+
 const got = require('got');
 const HttpAgent = require('agentkeepalive');
 const HttpsAgent = HttpAgent.HttpsAgent;
@@ -23,21 +24,18 @@ function api(path, opts) {
     'user-agent': 'adenin Now Assistant Connector, https://www.adenin.com/now-assistant'
   }, opts.headers);
 
-  if (opts.token) {
-    opts.headers.Authorization = `token ${opts.token}`;
-  }
+  if (opts.token) opts.headers.Authorization = `token ${opts.token}`;
 
   let url = /^http(s)\:\/\/?/.test(path) && opts.endpoint ? path : opts.endpoint + path;
   url += ('?token=' + opts.token);
-  
-  if (opts.stream) {
-    return got.stream(url, opts);
-  }
 
-  return got(url, opts).catch(err => {
+  if (opts.stream) return got.stream(url, opts);
+
+  return got(url, opts).catch((err) => {
     throw err;
   });
 }
+
 const helpers = [
   'get',
   'post',
@@ -47,15 +45,15 @@ const helpers = [
   'delete'
 ];
 
-api.stream = (url, opts) => apigot(url, Object.assign({}, opts, {
+api.stream = (url, opts) => got(url, Object.assign({}, opts, {
   json: false,
   stream: true
 }));
 
 for (const x of helpers) {
   const method = x.toUpperCase();
-  api[x] = (url, opts) => api(url, Object.assign({}, opts, { method }));
-  api.stream[x] = (url, opts) => api.stream(url, Object.assign({}, opts, { method }));
+  api[x] = (url, opts) => api(url, Object.assign({}, opts, {method}));
+  api.stream[x] = (url, opts) => api.stream(url, Object.assign({}, opts, {method}));
 }
 
 module.exports = api;
